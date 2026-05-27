c33ee806ded021f2eb8b8015c656cba8d8790a8a
ArsalanNury <arsiiiland@yahoo.com>
2026-05-27 13:00:29 +0330

    Multi dimentional array

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int[,] multiDimentional = new int[4, 4]
+{
+    {1,2,3,4},
+    {5,6,7,8},
+    {9,10,11,12},
+    {13,14,15,16}
+};
+
+var rows = multiDimentional.GetLength(0);
+var columns = multiDimentional.GetLength(1);
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(multiDimentional[i, j] + " ");
+    }
+Console.WriteLine();
+}
