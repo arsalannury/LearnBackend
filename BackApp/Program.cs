1bd3b32a52c63b9c0b10bb2f526e028314b26739
ArsalanNury <arsiiiland@yahoo.com>
2026-06-13 18:15:53 +0330

    - Create class with different types to train OOP

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,109 +1,34 @@
-﻿using System;
+﻿Car Peogeout = new Car("206", "Blue Sky", 25000000, 1385);
+string warranty = Car.CheckWarranty(10011012);
+Console.WriteLine(warranty);
 
-namespace BackApp
+
+public class Car
 {
-    internal class Converter
+    readonly string Name;
+    readonly string Color;
+    readonly Int32 Price;
+    readonly Int16 YearOfCreate;
+    static readonly int[] ValidSeries = new int[10] { 10011012, 10011013, 10011014, 10011015, 10011016, 100110117, 10011018, 10011019, 10011020, 10011021 };
+
+    public Car(string name, string color, Int32 price, Int16 yearOfCreate)
     {
+        Name = name;
+        Color = color;
+        Price = price;
+        YearOfCreate = yearOfCreate;
+    }
 
-       private readonly static string[] options = new string[2] { "1- Remove all letters", "2- Show all letters" };
-       private readonly static List<string> letters = new List<string>();
-
-        public static void Main(string[] args)
+    public static string CheckWarranty(int seriesNumber)
+    {
+        foreach (var series in ValidSeries)
         {
-            do
+            if (seriesNumber == series)
             {
-                Console.WriteLine("Press S if you want to start application and Press Q if you want go to settings");
-
-                //Console.WriteLine("Press ESC if you want to close the applicaton");
-                //bool mustEnd = Console.ReadKey().Key == ConsoleKey.Escape;
-                //if (mustEnd) return;
-
-                string appType = Console.ReadLine() ?? "";
-                bool isAppTypeChar = char.TryParse(appType, out char result);
-
-                // TODO should check case 
-                if (isAppTypeChar)
-                {
-                    if (result.Equals('s'))
-                    {
-                        MainApplication();
-                        continue;
-                    }
-                    else if (result.Equals('q'))
-                    {
-                        SettingApplication();
-                    }
-                }
-
-            } while (true);
-        }
-
-
-
-        // helper methods to work with main app
-       static void SettingApplication()
-        {
-            Console.WriteLine("You can enter 1 or 2 instead of enter letters");
-            foreach (string item in options)
-            {
-                Console.WriteLine(item);
-            }
-
-            string settingOption = Console.ReadLine() ?? string.Empty;
-            bool isSettingOptionValid = int.TryParse(settingOption, out int result);
-            if (isSettingOptionValid)
-            {
-                if (result == 1)
-                {
-                    letters.Clear();
-                    Console.WriteLine("All letters removed successfully");
-                }
-                else if (result == 2)
-                {
-                    foreach (string item in letters)
-                    {
-                        Console.WriteLine(item);
-                    }
-                }
+                return "Your warranty still remains for 3 years or 20000 kilometer, " +
+                       "check this link below to find more information about your car";
             }
         }
-
-       static void MainApplication()
-        {
-            StarterLogger();
-
-            // get input from user
-            string userSuggestion = Console.ReadLine() ?? string.Empty;
-            string userSuggestionForCheck = userSuggestion.Replace(" ", string.Empty).ToLower();
-
-            int duplicateLetterIndex = letters.FindIndex(lett => lett.Replace(" ", string.Empty) == userSuggestionForCheck);
-
-            // should pass the fill string or the string under the 50 length
-            if (userSuggestion == string.Empty || userSuggestion.Length > 50)
-            {
-                Console.Clear();
-                return;
-            }
-
-            // letters and characters can not be duplicate
-            if (duplicateLetterIndex != -1)
-            {
-                Console.WriteLine("You can not add duplicated letters, try another one");
-                return;
-            }
-
-            // add to list with convert all word to lower -> A TEST TExt will be a test text
-            letters.Add(userSuggestion.ToLower());
-            Console.WriteLine("The item added successfully");
-        }
-
-        static void StarterLogger()
-        {
-            Console.WriteLine("- - - - - - - - - - - - - - - - ");
-            Console.WriteLine("Enter your suggestion letters to add to the archive");
-            Console.WriteLine("- - - - - - - - - - - - - - - - ");
-            Console.WriteLine("\n");
-        }
-
+        return "Car warranty not found! Please check the series number";
     }
 }
