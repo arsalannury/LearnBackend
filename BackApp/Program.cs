15c28249c9612d95fbd0f01f4e4765cc1f342c58
ArsalanNury <arsiiiland@yahoo.com>
2026-06-18 22:36:23 +0330

    - Overloading method

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -7,11 +7,8 @@
             Console.WriteLine("Hello World From OOP!");     
         }
 
-        public int CalculateSum(Int16 x, Int16 y) => x + y;
-        public void CalculateSum(Int32 x , Int32 y)
-        {
-            Console.WriteLine($"{x} {y}");
-        }
+        public int CalculateSum(int x, int y) => x + y;
+        public int CalculateSum(int x , int y, int z) => x + y + z;
 
     }
 }
