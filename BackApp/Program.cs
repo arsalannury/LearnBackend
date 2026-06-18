1a1204f49d53e60f5b7ec7434a802f5d916d5062
ArsalanNury <arsiiiland@yahoo.com>
2026-06-18 22:34:00 +0330

    - Overloading methods

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,34 +1,17 @@
-﻿Car Peogeout = new Car("206", "Blue Sky", 25000000, 1385);
-string warranty = Car.CheckWarranty(10011012);
-Console.WriteLine(warranty);
-
-
-public class Car
+﻿namespace BackApp
 {
-    readonly string Name;
-    readonly string Color;
-    readonly Int32 Price;
-    readonly Int16 YearOfCreate;
-    static readonly int[] ValidSeries = new int[10] { 10011012, 10011013, 10011014, 10011015, 10011016, 100110117, 10011018, 10011019, 10011020, 10011021 };
-
-    public Car(string name, string color, Int32 price, Int16 yearOfCreate)
+    internal class Program
     {
-        Name = name;
-        Color = color;
-        Price = price;
-        YearOfCreate = yearOfCreate;
-    }
-
-    public static string CheckWarranty(int seriesNumber)
-    {
-        foreach (var series in ValidSeries)
+        static void Main(string[] args)
         {
-            if (seriesNumber == series)
-            {
-                return "Your warranty still remains for 3 years or 20000 kilometer, " +
-                       "check this link below to find more information about your car";
-            }
+            Console.WriteLine("Hello World From OOP!");     
         }
-        return "Car warranty not found! Please check the series number";
+
+        public int CalculateSum(Int16 x, Int16 y) => x + y;
+        public void CalculateSum(Int32 x , Int32 y)
+        {
+            Console.WriteLine($"{x} {y}");
+        };
+
     }
 }
