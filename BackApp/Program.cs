90b3e4ffa0031cb15bde464826cc942c20efcd41
ArsalanNury <arsiiiland@yahoo.com>
2026-06-25 23:33:07 +0330

    - Train multi demotional arrays

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -14,7 +14,7 @@ else
     Console.WriteLine(parsedValidInt * 2);
 }*/
 
-
+/*
 char[] bloodGroups = new char[3] {'Q','E','O'};
 Console.WriteLine(bloodGroups.Length);
 
@@ -23,5 +23,25 @@ foreach(char c in bloodGroups)
     string check = c == bloodGroups[^1] ? "" : ",";
 
     Console.Write($"{c} {check} ");
-}
+}*/
 
+string[,] square = new string[2, 3]
+{
+    {"Mohammad","Fatemeh","Ali" },
+    {"Hasan","Hossein","Sajad"}
+};
+
+//Console.WriteLine(square[0,2]);
+
+for (int i = 0; i < square.GetLength(0); i++)
+{
+    for (int j = 0; j < square.GetLength(1); j++)
+    {
+        Console.Write($" {square[i, j]} ");
+
+        if (square[i, j] == square[0, 2])
+        {
+            Console.WriteLine("");
+        }
+    }
+}
