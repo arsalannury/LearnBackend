58e952a3d149f53b6f9cc224ca03b41ebf5b6810
ArsalanNury <arsiiiland@yahoo.com>
2026-06-25 23:23:23 +0330

    - Train string.All and arrays

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,51 +1,27 @@
-﻿string name;
-name = "Arsalan";
+﻿/*
+Console.WriteLine("Please enter a valid number:");
+Console.WriteLine("");
 
-string Name;
-Name = name;
+string userInput = Console.ReadLine();
 
-string @class = "A2";
-
-string @namespace = "BackApp"; // -> var @namespace = "BackApp";
-//var variable; ---> An implicitly typed local variable must be initialized
-
-//Console.WriteLine(@class);
-//Console.WriteLine(name);
-//Console.WriteLine(Name);
-//Console.WriteLine("Subscription:" + 10 * 2);
-//Console.WriteLine("Subscription:" + 10 + 2);
-//Console.WriteLine("Subscription:" + (10 + 2));
-
-//Test();
-void Test()
+if (!userInput.All(char.IsDigit))
 {
-    Console.WriteLine("TTTTest");
+    Console.WriteLine("You entered an invalid number");
+}
+else
+{
+    int parsedValidInt = int.Parse(userInput);
+    Console.WriteLine(parsedValidInt * 2);
+}*/
+
+
+char[] bloodGroups = new char[3] {'Q','E','O'};
+Console.WriteLine(bloodGroups.Length);
+
+foreach(char c in bloodGroups)
+{
+    string check = c == bloodGroups[^1] ? "" : ",";
+
+    Console.Write($"{c} {check} ");
 }
 
-
-string SwitchExpression(int arg)
-{
-   return arg switch
-    {
-        <= 0 => $"Ilegall age: {arg}",
-        <= 10 and > 0 => $"We prefer to don't accept: {arg}",
-        <= 50 and > 10 => $"We prefer to accept with some warnings: {arg}",
-        <= 100 and > 50 => $"We proud to accept some one like you: {arg}",
-        _ => "Pass age",
-    };
-}
-
-string resultOfExpression = SwitchExpression(90);
-//Console.WriteLine(resultOfExpression);
-
-
-char BloodGroup(int intiGen)
-{
-    return intiGen switch
-    {
-        > 0 and <= 5 => 'A',
-        > 5 and <= 10 => 'B',
-        > 10 and <= 15 => 'O',
-        _ => 'N',
-    };
-}
