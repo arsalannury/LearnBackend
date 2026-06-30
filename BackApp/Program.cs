740e9fd4e1523157953e22b5c02c5e8271e33c04
ArsalanNury <arsiiiland@yahoo.com>
2026-06-30 23:49:58 +0330

    - Car class access modif and naming conventions

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,9 +1,26 @@
-﻿class Car
+﻿Car pride = new Car("Pride 131", "Blue Sky", 4_500_000);
+
+Console.WriteLine(pride.CalculateCarPrice());
+
+class Car
 {
     // default constructor parameter less \\
-    public string Name; // -> default value is empty string
-    public string Color; // -> default value is empty string
-    public int Price; // -> default value is 0
+    string _name; // -> default value is empty string
+    string _color; // -> default value is empty string
+    int _price; // -> default value is 0
 
+    public Car(string name, string color, int price)
+    {
+        _name = name;
+        _color = color;
+        _price = price;
+    }
+
+    public int CalculateCarPrice()
+    {
+        int constantPriceBaseYear = 1000;
+
+        return (this._price * constantPriceBaseYear) / (DateTime.Now.Year - 2000);
+    }
 
 }
