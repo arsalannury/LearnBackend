3d662140972e3f458e32fe47192e0d7098043fa0
ArsalanNury <arsiiiland@yahoo.com>
2026-07-14 00:54:58 +0330

    try to learn

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -4,7 +4,27 @@
     {
         public static void Main(string[] args)
         {
+            Tank abraham = new Tank();
+        }
+    }
 
+    class Tank
+    {
+
+        public string Model;
+        public int YearBuild;
+
+        public Tank(string model, int yearBuld)
+        {
+            Console.WriteLine("2");
+            Model = model;
+            YearBuild = yearBuld;
+        }
+
+        public Tank() : this("Abraham", 1992)
+        {
+            Console.WriteLine("1");
+            YearBuild = 0;
         }
     }
 }
