04073d5e6023edc1ab3cc5ad1cc9d407d9ae1d30
ArsalanNury <arsiiiland@yahoo.com>
2026-07-14 22:26:37 +0330

    Pass Class As Argument

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -4,41 +4,26 @@
     {
         public static void Main(string[] args)
         {
-            DateTime now = DateTime.Now;
-            var appointment = new Appointment(now, "Mohadeseh Sanaei Nezhad");
-            appointment.Reschedule(10);
-            Console.WriteLine($"{appointment.Date} - {appointment.Name} - {appointment.DoctorName}");
         }
     }
 
-    class Appointment
+    class A
     {
-        public DateTime Date;
-        public string Name;
-        public string DoctorName;
-
-        public Appointment(DateTime date, string name, string doctorName)
+        public void Foo(B instanceB)
         {
-            Date = date;
-            Name = name;
-            DoctorName = doctorName;
-        }
-
-        public Appointment(DateTime date, string name) : this(date, name, "Arsalan Nury")
-        {
-            Date = date;
-            Name = name;
-        }
-
-        public void Reschedule(int addDays, string name)
-        {
-            Date = Date.AddDays(addDays);
-            Name = name;
-        }
-
-        public void Reschedule(int addDays)
-        {
-            Date = Date.AddDays(addDays);
+            Console.WriteLine(instanceB);
         }
     }
+
+    class B
+    {
+        public string Property = "Test";
+
+        public void Test()
+        {
+            var instance = new A();
+            instance.Foo(this);
+        }
+    }
+
 }
