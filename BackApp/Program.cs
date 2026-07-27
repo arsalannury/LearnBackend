be3d65427f8ebf30aa5e6fb51fa5b849734b7a54
ArsalanNury <arsiiiland@yahoo.com>
2026-07-27 04:42:56 +0330

    Use Instance inside Instance

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -4,46 +4,27 @@
     {
         public static void Main(string[] args)
         {
-            var russianGun = new Wepeon("AK 47");
-            Console.WriteLine(russianGun.GetSummaryWepeon());
-
-            var m4Gun = new Wepeon("M4 Tactic", 1954);
-            Console.WriteLine(m4Gun.GetSummaryWepeon());
+           
         }
     }
 
-    class Wepeon
+    class Holding
     {
-        private readonly string _name;
-        private readonly int _yearOfBuild;
-        private readonly int _bulletCounts;
-        private readonly string _color;
-        private readonly string _style;
-
-
-        public Wepeon(string name, int buildYear, int bulletCount, string color, string shootStyle)
+        public void Members(Company company)
         {
-            _name = name;
-            _yearOfBuild = buildYear;
-            _bulletCounts = bulletCount;
-            _color = color;
-            _style = shootStyle;
+            Console.WriteLine(company.Name);
         }
+    }
 
-        public Wepeon(string name, int buildYear)
+    class Company
+    {
+        public string Name = "DigiKala";
+
+        public void CompnayRoot()
         {
-            Console.WriteLine("Run 1");
-            _name = name;
-            _yearOfBuild = buildYear;
+            var holding = new Holding();
+            holding.Members(this);
         }
-
-        public Wepeon(string name) : this(name, 1902)
-        {
-            Console.WriteLine("Run 2");
-            _name = name;
-        }
-
-        public string GetSummaryWepeon() => $"{_name} - {_yearOfBuild}";
     }
 
 }
