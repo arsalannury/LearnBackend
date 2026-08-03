8a6cb9143684502bc87d8c235d09a71ca841140c
ArsalanNury <arsiiiland@yahoo.com>
2026-08-03 14:17:54 +0330

    Readonly and Constant

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args)
         {
+            var t = new Iran();
 
         }
     }
@@ -11,9 +12,16 @@
 
     class Iran
     {
+        public const int sum = 10 * 10;
         public readonly string Capital = "Tehran";
         public readonly int Population = 91;
         public readonly string[] Neighbors = new string[6] { "Iraq", "Pakistan", "Afghanistan", "Turkamanistan", "Armenia", "Turkey" };
         public readonly string[] Religions = new string[6] { "Islam", "Christianity", "Judaism", "Zoroastrianism", "Druze", "bahaism" };
+
+        public static int Calc()
+        {
+            return 10 * 10;
+        }
     }
+
 }
