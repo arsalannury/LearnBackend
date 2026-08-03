416284f55621fe530c4f3fa9e0629a1976ecb69b
ArsalanNury <arsiiiland@yahoo.com>
2026-08-03 12:54:38 +0330

    Readonly

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace BackApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    class Iran
+    {
+        public readonly string Capital = "Tehran";
+        public readonly int Population = 91;
+        public readonly string[] Neighbors = new string[6] { "Iraq", "Pakistan", "Afghanistan", "Turkamanistan", "Armenia", "Turkey" };
+        public readonly string[] Religions = new string[6] { "Islam", "Christianity", "Judaism", "Zoroastrianism", "Druze", "bahaism" };
+    }
+}
