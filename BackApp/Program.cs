c85e495ed0e3dfa9b116557d989e86f5bc958f24
ArsalanNury <arsiiiland@yahoo.com>
2026-09-01 15:45:17 +0330

    Variables precedense

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿int first = 100;
 int second = 101;
 
-Console.WriteLine("The result is:" + (first+second));
+//Console.WriteLine("The result is:" + (first+second));
+//Console.WriteLine("The result is:" + first - second);
+//Console.WriteLine("The result is:" + (first - second));
 
-Console.WriteLine("The result is:" + first - second);
+//Console.WriteLine("The result is:" + second / first);
+//Console.WriteLine("The result is:" + second * first);
