d5302939a447c5998299d8105cc4cf11adf77e52
ArsalanNury <arsiiiland@yahoo.com>
2026-09-03 03:06:29 +0330

    Switch case again

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,10 +1,50 @@
-﻿bool Toggle()
+﻿
+
+while (true)
 {
-    var @default = false;
-    @default = !@default;
+    Console.WriteLine("Pick a number from 1 to 5 to see what you can eat today");
+    var userInput = Console.ReadLine();
+    bool isParsed = int.TryParse(userInput, out int digitUserInput);
+
+    if (digitUserInput != 0 && digitUserInput <= 5)
+    {
+        string meal = Meal(digitUserInput);
+
+        Console.WriteLine("-----------------");
+        Console.WriteLine($"Your meal for now is {meal}");
+        Console.WriteLine("-----------------");
+        continue;
+    }
 
 
-    return @default;
+        continue; 
 }
 
-Console.WriteLine(Toggle());
+string Meal(int digitUserInput)
+{
+    string mealName = string.Empty;
+
+    switch (digitUserInput)
+    {
+        case 1:
+            mealName = "Chicken with carrot";
+            break;
+        case 2:
+            mealName = "Wafell with strawberry";
+            break;
+        case 3:
+            mealName = "Rise with fish";
+            break;
+        case 4:
+            mealName = "Fish and chips";
+            break;
+        case 5:
+            mealName = "Omelet";
+            break;
+        default:
+            mealName = string.Empty;
+            break;
+    }
+
+    return mealName;
+}
