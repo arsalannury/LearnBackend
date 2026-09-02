5063d4db8ace2bf68755df74de73bfd729f019ef
ArsalanNury <arsiiiland@yahoo.com>
2026-09-03 02:21:39 +0330

    Toggle

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,25 +1,10 @@
-﻿bool CanStudentStartNewSession(double grade, out string schoolMessage)
+﻿bool Toggle()
 {
-    schoolMessage = string.Empty;
-    bool res = false;
+    var @default = false;
+    @default = !@default;
 
-    switch (grade)
-    {
-        case <= 9.5:
-            schoolMessage = "You must be better than what you are now. the life is too hard";
-            res = false;
-            break;
-        case > 9.5:
-            schoolMessage = "Congratulations student! you did the best";
-            res = true;
-            break;
-        default:
-            res = false;
-            break;
-    }
 
-    return res;
+    return @default;
 }
 
-bool checkArsalan = CanStudentStartNewSession(8, out string message);
-Console.WriteLine(message);
+Console.WriteLine(Toggle());
