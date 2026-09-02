2a2d8b8af061ee02b499738517f31c5f71fcbe1e
ArsalanNury <arsiiiland@yahoo.com>
2026-09-03 01:39:11 +0330

    String interpolation

--- BackApp/Program.cs
+++ BackApp/Program.cs
@@ -1,13 +1,8 @@
-﻿
-string password = Console.ReadLine() ?? string.Empty;
-bool isParsedPassword = int.TryParse(password, out var parsed);
-
-if (!isParsedPassword && parsed == 0)
+﻿string OutputOriganizer(string command)
 {
-    if (password.Trim().Length < 10)
-    {
-        Console.WriteLine("-----------------------");
-        Console.WriteLine("Select a longer password to continue");
-        Console.WriteLine("-----------------------");
-    } 
+    return $"User typed a command in terminal with following details: {command}";
 }
+
+
+var userOut = OutputOriganizer("remove old applications from your system");
+Console.WriteLine(userOut);
